4c63d35bf4bcde5dc13646c78bb6d412c071bbfd
Serhiy Khalak <serghalak@gmail.com>
2019-11-20 12:59:41 +0200

    first commit

---
+++ salon-web/src/test/java/salon/SalonBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
