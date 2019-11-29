d1763f30506d218b20a33e2d5d791a306d0927bb
Serhiy Khalak <serghalak@gmail.com>
2019-11-29 15:04:47 +0200

    SecurityConfig implements WebSecurityConfigurerAdapter #34

--- salon-data/src/main/java/com/salon/security/SecurityConfig.java
+++ salon-data/src/main/java/com/salon/security/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-//@Configuration
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
